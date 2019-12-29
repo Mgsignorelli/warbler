d39074fa83614511aed1b939c95cb0efb147c3a7
Micaela Signorelli <m.signorelli@gmail.com>
2019-12-29 16:43:44 +0000

    Adds proper testing framework and configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.scss';
-import {Container} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 
 function App() {
   return (
-    <Container/>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Warbler</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders header', () => {
+    const {getByText} = render(<App/>);
+    const headerElement = getByText(/warbler/i);
+
+    expect(headerElement).toBeInTheDocument();
+  });
+
 });
